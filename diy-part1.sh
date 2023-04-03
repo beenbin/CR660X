fd499108302e572dd00c0bfb15cc538a068b7afb
Bingo <61998928+beenbin@users.noreply.github.com>
2023-04-03 22:44:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,7 @@
 #
 
 # Uncomment a feed source
-sed -i '68s/||SOFT_FLOAT//' ./feeds/packages/lang/node/Makefile
+sed -i '68s/||SOFT_FLOAT//' feeds/packages/lang/node/Makefile
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 #sudo apt install libssl1.0-dev #降级OPENSSL
